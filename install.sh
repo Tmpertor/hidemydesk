07d8a6b0d1a39fff16573fa915eee29aa344040c
Taguar258 <36562445+Taguar258@users.noreply.github.com>
2020-02-09 15:54:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ cp src/hidemydesk /usr/local/bin/hidemydesk
 cp src/hidemydesk2 /Applications/hidemydesk
 chmod 777 /usr/local/bin/hidemydesk
 chmod 777 /Applications/hidemydesk
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/hidemydesk</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+killall Dock
+
+echo "Thanks for installing."
+echo "Made by Taguar258"
