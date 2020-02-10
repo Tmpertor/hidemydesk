76d21514b126a1e98d457ea04291318a08c7f28c
Taguar258 <36562445+Taguar258@users.noreply.github.com>
2020-02-10 12:55:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 chmod +x uninstall.sh
-chmod +x src/hidemydesk
-chmod +x src/hidemydesk
+chmod +x src/hidemydesk.app
+chmod +x src/hidemydesk.app/Content/MacOS/hidemydesk
 cp src/hidemydesk /usr/local/bin/hidemydesk
-cp src/hidemydesk2 /Applications/hidemydesk
+cp -R src/hidemydesk.app /Applications/hidemydesk.app
 chmod 777 /usr/local/bin/hidemydesk
-chmod 777 /Applications/hidemydesk
+chmod 777 /Applications/hidemydesk.app
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/hidemydesk</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 killall Dock
 
